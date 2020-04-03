d9f7919ce70d56fa0a400d89823e121dc3856463
meimei <5474@ait.nsw.edu.au>
2020-04-03 12:38:26 +1100

    added guess limit logic

--- App.js
+++ App.js
@@ -6,8 +6,12 @@ export default class Game extends Component {
   state = {
     secret : 0,
     input: '',
-    feedback: ''
+    feedback: '',
+    limit: 5,
+    turns: 1,
+    feedback2: ''
   }
+
   //function to pick a random number
   generateRandom() {
     return Math.round( Math.random() * 100 )
@@ -15,7 +19,8 @@ export default class Game extends Component {
   //function to initialise the game
   init() {
     const secretNumber = this.generateRandom()
-    this.setState({ secret: secretNumber })
+    this.setState({ secret: secretNumber
+    })
   }
 //lifecycle function
 componentDidMount() {
@@ -24,29 +29,55 @@ componentDidMount() {
 
 //update input state
 updateInput = (value) => { this.setState({ input: value }) }
+increment = () => { this.setState({turns: this.state.turns + 1})}
 
 checkGuess = () => {
+
   const userGuess = parseInt(this.state.input);
-  const secretNumber = this.secret;
-  if( userGuess == secretNumber ) {
-    this.setState({ feedback: ' You guessed right, the number is ' + secretNumber })
-    //restart the game
-    this.init()
-    return
+  const secretNumber = this.state.secret;
+  const guessLimit = this.state.limit;
+  const guessCount = this.state.turns;
+
+  if( guessCount < guessLimit ){
+
+      if( userGuess < secretNumber ) {
+      this.setState({ feedback: ' The number is larger than ' + userGuess })
+      this.setState({ feedback2: ' Guess ' + guessCount })
+      return
+      }
+
+      if( userGuess > secretNumber ){
+      this.setState({ feedback: ' The number is smaller than ' + userGuess })
+      this.setState({ feedback2: ' Guess ' + guessCount })
+      return
+      }
+
+      if(userGuess == secretNumber){
+      this.setState({ feedback: ' You guessed right, the number is ' + secretNumber})
+      this.setState({ feedback2: ' Guess ' + guessCount })
+    
+      //restart the game
+      this.init()
+      return
+      }
   }
-  if( userGuess < secretNumber ) {
-    this.setState({ feedback: ' The number is larger than ' + userGuess })
+  else{
+    this.setState({ feedback: ' You run out of guesses. The correct number is ' + secretNumber })
+    this.setState({ feedback2: '' })
+     //restart the game
+     this.init()
+     return
   }
-  if( userGuess > secretNumber ){
-    this.setState({ feedback: ' The number id smaller than ' + userGuess })
-  }
-  return
 }
+
+  
+
 
 render () {
   return(
     <View style={styles.container}>
       <Text>Guess my number, now!</Text>
+      <Text>You have 5 tries to guess the number correct</Text>
       <TextInput
       style={styles.input}
       keyboardType='number-pad'
@@ -54,10 +85,12 @@ render () {
       </TextInput>
       <TouchableHighlight style={styles.button}
       underlayColor = 'white'
+      onPressIn={this.increment}
       onPress={this.checkGuess}>
         <Text>Submit Guess</Text>
       </TouchableHighlight>
   <Text>{this.state.feedback}</Text>
+  <Text>{this.state.feedback2}</Text>
     </View>
   )
 }
